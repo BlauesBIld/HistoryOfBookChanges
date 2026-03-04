9d003cf6e6c84551adc4d133c1fd876b743f76f2
BlauesBild <dejo13@gmx.at>
2026-03-04 18:48:35 +0100

    changed description in book entity to nullable

--- TechnicalTask/Entities/Book.cs
+++ TechnicalTask/Entities/Book.cs
@@ -17,7 +17,7 @@ public class Book
 
     public Guid Id { get; set; }
     public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime PublishDate { get; set; }
     public List<Author> Authors { get; set; }
 }
